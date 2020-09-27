081826f6e925899e00b534bc7a637966854ebb12
Claudinei Bido <bido.c@live.com>
2020-09-27 13:49:03 +0200

    interacting with DOM elements

--- dist/app.js
+++ dist/app.js
@@ -5,13 +5,22 @@ class ProjectInput {
         this.hostElement = document.querySelector(`${projectHost}`);
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionTextAreaElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        this.configure();
         this.attach();
+    }
+    configure() {
+        this.element.addEventListener('submit', (event) => {
+            event.preventDefault();
+            console.log(this.titleInputElement.value);
+        });
     }
     attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
     }
 }
 const prjInput = new ProjectInput('#project-input', '#app');
-const prInput = new ProjectInput('#project-input', '#app');
 console.log(prjInput);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,35 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
+    titleInputElement: HTMLInputElement;
+    descriptionTextAreaElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
 
   constructor(projectInput: string, projectHost: string) {
       this.templateElement = document.querySelector(`${projectInput}`)! as HTMLTemplateElement;
       this.hostElement = document.querySelector(`${projectHost}`)! as HTMLDivElement;
 
       //second argument reffers to create a deep copy
+      //https://developer.mozilla.org/en-US/docs/Web/API/Document/importNode
       const importedNode = document.importNode(this.templateElement.content, true);
       this.element = importedNode.firstElementChild as HTMLFormElement;
+      this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+      this.descriptionTextAreaElement = this.element.querySelector('#description') as HTMLInputElement;
+      this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement; 
+
+      this.configure();
       this.attach();
   }
+    private configure() {
+        this.element.addEventListener('submit', (event) => {
+            event.preventDefault();
+            console.log(this.titleInputElement.value)
+        })
+    }
     private attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element)
     }
 }
 
 const prjInput = new ProjectInput('#project-input', '#app');
-const prInput = new ProjectInput('#project-input', '#app');
 console.log(prjInput)
